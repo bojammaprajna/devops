be3c15bc534e01179aa2ebaa18979d2f60ba0663
bojammaprajna <bojammaprajna@gmail.com>
2024-07-12 15:09:12 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,8 +1,5 @@
-ef average( a , n ): 
-
-	# Find sum of array element 
-	sum = 0
-	for i in range(n): 
-		sum += a[i] 
-	
-	return sum/n; 
+class hello {  
+    public static void main(String args[]){  
+     System.out.println("Hello Java");  
+    }  
+}  
